9feda03f0a34182e7a989798835dd17fc47e470d
Joao M. Almeida <Joao-M-Almeida@users.noreply.github.com>
2015-10-27 16:34:56 +0000

    Step Analysis, now averaged values of the sensors, Matlab script

--- Arduino/Altered_lab_code/Step_script.m
+++ Arduino/Altered_lab_code/Step_script.m
@@ -1,12 +1,30 @@
 %% Arduino Step Response Scritp
+clear
+clc
 
-arduino_ini();
+file='output.mat';
+
+%% Init
+arduino_ini()
+pause(3)
+
+% Send step, save data
+arduino_cmd({'s','<'})
+
+arduino_end()
+%% load the data...
+
+load(file);
+
+%% plot just the step
+p=figure();
+hold on
+plot(y(:,end)-y(1,end),y(:,2), '.-')
+
+%%  Calculations
+figure(p)
+plot(y(:,end)-y(1,end),ones(1,length(y))*0.63*max(y(:,2)))
+
 
 %%
-arduino_cmd('s');
 
-arduino_interf('get_array', 'step.txt')
-
-%%
-
-arduino_end();
