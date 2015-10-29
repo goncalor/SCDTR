b5c327c16002f3644649d4f028bf699b2a2b6470
Joao M. Almeida <Joao-M-Almeida@users.noreply.github.com>
2015-10-29 12:42:20 +0000

    Tests and Feedfoward

--- Arduino/Altered_lab_code/ctrl4_serial/ctrl4_serial.ino
+++ Arduino/Altered_lab_code/ctrl4_serial/ctrl4_serial.ino
@@ -14,6 +14,7 @@
 #define INTEGRAL_TIME 0.001
 #define GAIN_K 2.0	
 #define GAIN_D 0
+#define FEEDFOWARD_U 50
 
 const int analogInPin = 0;  // Analog input pin that ldr
 //const int analogOutPin = 9; // Analog output pin that the LED is attached to
@@ -32,6 +33,7 @@ int ctrl_ref=127, ctrl_e, ctrl_u, ctrl_y;
 //double a0=2.0, a1=0.0, a2=0.0, b1=0.0, b2=0.0;
 unsigned int Ts= SAMPLE_TIME; // 1 msec
 double gain_k = GAIN_K, integral_time=INTEGRAL_TIME, gain_d=GAIN_D, ctrl_wind_gain=1/INTEGRAL_TIME;
+double feedfoward_u = FEEDFOWARD_U;
 int loopMode= 0, loopOutputFlag= 1;
 int ctrl_verbose_flag= 0; //1;
 
@@ -224,7 +226,7 @@ void ctrl_loop() {
 	unsigned long write_time, end_time,start_time,t0,ctrl_ui=0,ctrl_ui_before=0;
 	long delay_time;
 	
-	Serial.println("[write_t,\t ctrl_u,\t ctrl_y,\t ctrl_e,\t ctrl_ui,\t ctrl_e_sat,\t ctrl_ref,\t sample_time]");
+	Serial.println("write_t ctrl_u ctrl_y ctrl_e ctrl_ui ctrl_e_sat ctrl_ref");
 	t0=micros();
 	while(1){
 		start_time = micros();
@@ -235,36 +237,45 @@ void ctrl_loop() {
 		/*Proportinal-Integral*/
 		/*ctrl_ui = ctrl_ui_before + Ts/(integral_time*1000000) * ctrl_e;
 		ctrl_ui_before = ctrl_ui;
-		full_ctrl_u = gain_k * ctrl_e + gain_k*integral_time * (ctrl_ui + ctrl_e);
+		full_ctrl_u = gain_k * ctrl_e + gain_k*integral_time * ctrl_ui;
 		full_ctrl_u = map(full_ctrl_u, 0, 1023, 0, 255); // Doesn't constrain to within range
 		ctrl_u = constrain(full_ctrl_u, 0, 255);*/
 		
 		/*Proportinal-Integral with Anti Windup*/ // Page307 chapter 10
-		ctrl_ui = ctrl_ui_before + Ts/(integral_time*1000000) * (ctrl_e+ctrl_e_sat*ctrl_wind_gain);
+		/*ctrl_ui = ctrl_ui_before + Ts/(integral_time*1000000) * (ctrl_e+ctrl_e_sat*ctrl_wind_gain);
 		ctrl_ui_before = ctrl_ui;
 		full_ctrl_u = gain_k * ctrl_e + gain_k*integral_time * ctrl_ui ;
+		full_ctrl_u = map(full_ctrl_u, 0, 1023, 0, 255); // Doesn't constrain to within range
+		ctrl_u = constrain(full_ctrl_u, 0, 255);
+		ctrl_e_sat = full_ctrl_u-ctrl_u;*/
+		
+		/*Proportinal-Integral with Anti Windup and feedfoward*/ // Page307 chapter 10
+		ctrl_ui = ctrl_ui_before + Ts/(integral_time*1000000) * (ctrl_e+ctrl_e_sat*ctrl_wind_gain);
+		ctrl_ui_before = ctrl_ui;
+		full_ctrl_u = gain_k * ctrl_e + gain_k*integral_time * ctrl_ui + feedfoward_u ;// now add feedfoward
 		full_ctrl_u = map(full_ctrl_u, 0, 1023, 0, 255); // Doesn't constrain to within range
 		ctrl_u = constrain(full_ctrl_u, 0, 255);
 		ctrl_e_sat = full_ctrl_u-ctrl_u;
 		
 		
 		
+		
 
-		write_time = micros();
+		write_time = micros();	
 		analogWrite(analogOutPin, ctrl_u);
 
 		Serial.print(write_time-t0);
-		Serial.print(",\t");
+		Serial.print(" ");
 		Serial.print(full_ctrl_u);
-		Serial.print(",\t ");
+		Serial.print(" ");
 		Serial.print(ctrl_y);
-		Serial.print(",\t ");
+		Serial.print(" ");
 		Serial.print(ctrl_e);
-		Serial.print(",\t ");
+		Serial.print(" ");
 		Serial.print(ctrl_ui);
-		Serial.print(",\t ");
+		Serial.print(" ");
 		Serial.print(ctrl_e_sat);
-		Serial.print(",\t ");
+		Serial.print(" ");
 		Serial.println(map(ctrl_ref, 0, 254, 0, 1023));
 		//Serial.print(",\t");
 
