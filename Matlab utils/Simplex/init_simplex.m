21b8d6c7e4a22f603a3c9dec28bdaafbe0008a55
Joao M. Almeida <Joao-M-Almeida@users.noreply.github.com>
2015-11-18 18:13:16 +0000

    Draft of init_simplex. Untested

---
+++ Matlab utils/Simplex/init_simplex.m
@@ -0,0 +1,24 @@
+function [ N,B,A,b,c,v] = init_simplex( A,b,c )
+% [ N,B,A,b,c,v] = INIT_SIMPLEX( A,b,c )
+% Basic init simplex to check if first solution is
+% feasible.
+b_min = b(1);
+k=1;
+v=0;
+[m,n]=size(A);
+for i=1:length(b)
+    if b(i)<b_min
+        b_min=b(i);
+        k=i;
+    end
+end
+
+if b(k)>0
+    N=1:n;
+    B=n+1:n+m;
+    return
+else
+    disp('ERROR: Unfeasible!!')
+end
+end
+
