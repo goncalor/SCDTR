3b2f4b0fdb813b1a1427f7d03515938937cecf49
Joao M. Almeida <Joao-M-Almeida@users.noreply.github.com>
2015-11-27 08:58:54 +0000

    more indentation fixes

--- lab10/find_i2c_slaves/find_i2c_slaves.ino
+++ lab10/find_i2c_slaves/find_i2c_slaves.ino
@@ -6,36 +6,34 @@
 
 #include <Wire.h>
 
-void setup()
-{
-//Serial.begin (115200);
-Serial.begin (9600);
-Serial.println ();
-Serial.println ("I2C scanner. Scanning ...");
-byte count = 0;
- 
-// Wire.begin(); // use this line to be just master
-Wire.begin(40); // use this line to be master or slave40
+const byte MY_ADDRESS = 40; // me
 
-for (byte i = 1; i < 120; i++)
-{
-  Wire.beginTransmission (i);
-  if (Wire.endTransmission () == 0)
-    {
-    Serial.print ("Found address: ");
-    Serial.print (i, DEC);
-    Serial.print (" (0x");
-    Serial.print (i, HEX);
-    Serial.println (")");
-    count++;
+void setup(){
+  //Serial.begin (115200);
+  Serial.begin (9600);
+  Serial.println ();
+  Serial.println ("I2C scanner. Scanning ...");
+  byte count = 0;
+
+  // Wire.begin(); // use this line to be just master
+  Wire.begin(MY_ADDRESS); // use this line to be master or slave40
+
+  for (byte i = 1; i < 120; i++){
+    Wire.beginTransmission (i);
+    if (Wire.endTransmission () == 0){
+      Serial.print ("Found address: ");
+      Serial.print (i, DEC);
+      Serial.print (" (0x");
+      Serial.print (i, HEX);
+      Serial.println (")");
+      count++;
     } // end of good response
-   delay (5);  // give devices time to recover
-} // end of for loop
-Serial.println ("Done.");
-Serial.print ("Found ");
-Serial.print (count, DEC);
-Serial.println (" device(s).");
+    delay (5);  // give devices time to recover
+  } // end of for loop
+  Serial.println ("Done.");
+  Serial.print ("Found ");
+  Serial.print (count, DEC);
+  Serial.println (" device(s).");
 }  // end of setup
 
 void loop() {} // do nothing
-
