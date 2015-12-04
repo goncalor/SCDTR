0d7f9adb0ad63ce5cc50bbbd7a236284443ef0b8
Joao-M-Almeida <jmirandadealme@gmail.com>
2015-12-04 09:28:57 +0000

    atom removing spaces

--- lab11/pc_serial/1_sync/serialwrite.cpp
+++ lab11/pc_serial/1_sync/serialwrite.cpp
@@ -1,5 +1,5 @@
 //SERIALWRITE.CPP
-// g++ -std=c++11 serialwrite.cpp –lpthread –lboost_system
+// g++ -std=c++11 serialwrite.cpp -lpthread -lboost_system
 
 #include <iostream>
 #include <boost/asio.hpp>
@@ -7,7 +7,7 @@ using namespace std;
 using namespace boost::asio;
 
 int main() {
-    io_service io;                //initialize services                  
+    io_service io;                //initialize services
     serial_port sp(io);        //create io object
     boost::system::error_code ec;
     sp.open("/dev/ttyACM0", ec);    //connect to port
@@ -15,10 +15,10 @@ int main() {
     //set baud rate
     sp.set_option(serial_port_base::baud_rate(9600),ec);
     if( ec ) {std::cout << "Error"; return -1;}
-    for (;;) {              
+    for (;;) {
        string str;
        getline(cin, str);
-       int nbytes = write(sp, boost::asio::buffer(str)); 
-       cout << "Wrote " << nbytes << " bytes to port" << endl; 
-    }  
-}  
+       int nbytes = write(sp, boost::asio::buffer(str));
+       cout << "Wrote " << nbytes << " bytes to port" << endl;
+    }
+}
