ced4bde6677cc3d054abedc7afe31431f8e54bc4
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2015-12-28 19:58:36 +0000

    Added more graphs for system characteristics.

--- report/part1/Deliverables/D2/save_plot.m
+++ report/part1/Deliverables/D2/save_plot.m
@@ -1,14 +1,14 @@
 data = dlmread("steadystate.csv", ",");
 
 plot(data(:,1), data(:,3));
-xlabel("PWM value (0 - 255)");
-ylabel("Voltage at A0 (V)");
+xlabel("PWM value (0--255)");
+ylabel("Voltage at \\texttt{A0} (\\si{\\volt})");
 axis([0, 255]);
 grid on
-set(gcf, "paperposition", [0 0 8.00000 6.00000]);
-set(gcf, "papersize", [8 6]);
-get(gcf);
-get(gca)
-#set(gca,'LooseInset',get(gca,'TightInset'));
-set(gca, 'LooseInset', [0.01, 0.07, 0.01, 0.01]);
-print("steady_state.pdf","-dpdf");
+#set(gcf, "paperposition", [0 0 8.00000 6.00000]);
+#set(gcf, "papersize", [8 6]);
+#get(gcf);
+#get(gca)
+##set(gca,'LooseInset',get(gca,'TightInset'));
+#set(gca, 'LooseInset', [0.01, 0.07, 0.01, 0.01]);
+print("steady_state", "-depslatex", "-mono");
